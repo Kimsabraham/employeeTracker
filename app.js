37aff363805ed4a9e931d8000cb7a5e68d3dadb9
Abraham Kim <krnpride730@yahoo.com>
2021-05-06 14:10:01 -0400

    SQL server

--- app.js
+++ app.js
@@ -2,3 +2,12 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
+// SQL connection
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "123",
+  database: "employeeTracker",
+});
+

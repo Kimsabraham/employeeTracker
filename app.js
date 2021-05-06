4b1ee612595660994bef5187f6baaeba5ad599d3
Abraham Kim <krnpride730@yahoo.com>
2021-05-06 15:10:31 -0400

    added app error and question prompt

--- app.js
+++ app.js
@@ -11,3 +11,29 @@ const connection = mysql.createConnection({
   database: "employeeTracker",
 });
 
+// starts application
+connection.connect(function (err) {
+  if (err) throw err;
+
+  questions();
+});
+
+// questions for the user
+function questions() {
+  inquirer
+    .prompt({
+      type: "list",
+      name: "action",
+      message: "Choose below.",
+      choices: [
+        "View All Employees",
+        "View All Employees by Department",
+        "View All Employees by Role",
+        "Create a Department",
+        "Create a Role",
+        "Add an Employee",
+        "Update Employee Role",
+        "Exit",
+      ],
+    })
+    

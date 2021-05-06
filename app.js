6c8c4ebf4982c0ce3d7003c05867c8ea05f373df
Abraham Kim <krnpride730@yahoo.com>
2021-05-06 13:56:59 -0400

    added mysql and inquier and started db

---
+++ app.js
@@ -0,0 +1,4 @@
+// dependencies
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
---
+++ db/employeeTrackerSchema.sql
@@ -0,0 +1,8 @@
+-- deletes exisiting database
+DROP DATABASE IF EXISTS employee_trackerDB;
+-- creates database
+CREATE database employee_trackerDB;
+
+-- use database
+USE employee_trackerDB;
+

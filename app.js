bd6e24bb6cea0e788c32215eec1c616d7b6543b0
Abraham Kim <krnpride730@yahoo.com>
2021-05-06 16:19:32 -0400

    function to view all employees

--- app.js
+++ app.js
@@ -72,3 +72,20 @@ function questions() {
     });
 }
 
+// view all employees
+function viewAll() {
+  connection.query(
+    `SELECT employee.first_name, employee.last_name, role.salary, role.title, department.name as "Department Name"
+    FROM employee_trackerDB.employee
+    INNER JOIN role ON employee.role_id = role.id
+    INNER JOIN department ON role.department_id = department.id`,
+
+    function (err, res) {
+      if (err) throw err;
+
+      console.table(res);
+      questions();
+    }
+  );
+}
+

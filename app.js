e264f0c2f605d33d2660a2e3adbd788dae587787
Abraham Kim <krnpride730@yahoo.com>
2021-05-06 19:30:50 -0400

    new employee function

--- app.js
+++ app.js
@@ -133,7 +133,7 @@ function viewDepartments() {
   );
 }
 
-// view  employees by role
+// view employees by role
 function viewRoles() {
   connection.query("SELECT role.title FROM employee_trackerDB.role", function (
     err,
@@ -177,7 +177,7 @@ function viewRoles() {
   });
 }
 
-// create a department
+// creates new department
 function newDepartment() {
   inquirer
     .prompt([
@@ -202,3 +202,121 @@ function newDepartment() {
     });
 }
 
+// create a new role
+function newRole() {
+  connection.query(
+    "SELECT department.name, department.id FROM employee_trackerDB.department",
+    function (err, res) {
+      if (err) throw err;
+
+      inquirer
+        .prompt([
+          {
+            name: "choice",
+            type: "list",
+            choices: function () {
+              var choiceArray = [];
+              var choiceArrayID = [];
+              for (var i = 0; i < res.length; i++) {
+                choiceArray.push(res[i].name);
+                choiceArrayID.push(res[i].id);
+              }
+              return choiceArray;
+            },
+            message: "Which Department?",
+          },
+          {
+            name: "title",
+            type: "input",
+            message: "What is the role name?",
+          },
+          {
+            name: "salary",
+            type: "input",
+            message: "What is the salary?",
+          },
+        ])
+        .then(function (answer) {
+          var department_id = answer.choice;
+
+          for (var i = 0; i < res.length; i++) {
+            if (res[i].name === answer.choice) {
+              department_id = res[i].id;
+              console.log(department_id);
+            }
+          }
+
+          connection.query(
+            "INSERT INTO role SET ?",
+            {
+              title: answer.title,
+              salary: answer.salary,
+              department_id: department_id,
+            },
+            function (err) {
+              if (err) throw err;
+
+              console.log(`You have created ${answer.title} with salary of ${answer.salary} in ${department_id}.`)
+
+              questions();
+            }
+          );
+        });
+    }
+  );
+}
+
+// add an employee
+function newEmployee() {
+  connection.query(
+    "SELECT role.title, role.id FROM employee_trackerDB.role",
+    function (err, res) {
+      if (err) throw err;
+
+      inquirer
+        .prompt([
+          {
+            name: "choice",
+            type: "list",
+            choices: function () {
+              var choiceArray = [];
+              for (var i = 0; i < res.length; i++) {
+                choiceArray.push(res[i].title);
+              }
+              return choiceArray;
+            },
+            message: "Which Role?",
+          },
+        ])
+        .then(function (answer) {
+          console.log(answer);
+          console.log(answer.choice);
+
+          var role_id = answer.choice;
+
+          for (var i = 0; i < res.length; i++) {
+            if (res[i].title === answer.choice) {
+              role_id = res[i].id;
+              console.log(role_id);
+            }
+          }
+
+          connection.query(
+            "INSERT INTO employee SET ?",
+            {
+              first_name: answer.first_name,
+              last_name: answer.last_name,
+              role_id: role_id,
+            },
+            function (err) {
+              if (err) throw err;
+
+              console.log(`You have created an employee ${answer.first_name} ${answer.last_name} with a role of ${role_id}.`)
+
+              questions();
+            }
+          );
+        });
+    }
+  );
+}

217b1e19f01a9dafb1e3d7d2771a42bfb3f7e0e5
Abraham Kim <krnpride730@yahoo.com>
2021-05-09 10:12:24 -0400

    update

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "July301993!",
+  password: "",
   database: "employee_trackerDB",
 });
 

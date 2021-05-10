980356aac4a112cd974d0656e3dffe60aaedfb68
Abraham Kim <krnpride730@yahoo.com>
2021-05-10 10:17:43 -0400

    added list of actions

--- app.js
+++ app.js
@@ -14,3 +14,21 @@ connection.connect(function (err) {
   if (err) throw err;
   runApp();
 });
+// List of actions
+function runApp() {
+  console.table("Employee!");
+  prompt({
+    name: "action",
+    type: "rawlist",
+    message: "Select a choice?",
+    choices: [
+      "View All Employees",
+      "View All Employees by Department",
+      "View All Employees by Role",
+      "Add a Department",
+      "Add a Role",
+      "Add an Employee",
+      "Update an Employee's Role",
+      "Exit",
+    ],
+   

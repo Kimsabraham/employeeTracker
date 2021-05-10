fffd88f6c8e70d86d46366cc654a02b1b937b777
Abraham Kim <krnpride730@yahoo.com>
2021-05-09 20:23:58 -0400

    created dependcies and port and connection

---
+++ app.js
@@ -0,0 +1,16 @@
+// Dependcies
+var mysql = require("mysql2");
+var { prompt } = require("inquirer");
+// SQL host
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "company_employeedb",
+});
+// connection
+connection.connect(function (err) {
+  if (err) throw err;
+  runApp();
+});

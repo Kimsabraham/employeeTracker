72ef1fad091a08856d2710421b1ee195d177e4de
Abraham Kim <krnpride730@yahoo.com>
2021-05-09 20:15:17 -0400

    updated table

--- db/employeeTrackerSchema.sql
+++ db/employeeTrackerSchema.sql
@@ -26,3 +26,36 @@ CREATE TABLE employee (
   PRIMARY KEY (id)
 );
 
+INSERT INTO department (name)
+VALUES ("Management"), 
+("Sales"),  
+("Finance"), 
+("Human Resources"),
+("Operations"), 
+("Research"),
+("Creative"),
+("Sales");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Manager", 60000, 1), 
+("Creative Director", 48000, 4), 
+("Marketing", 49000, 5),
+("Sales", 48000, 5),
+("Accountant", 43000, 8),
+("Operations", 40000, 8), 
+("Research", 85000, 9),
+("HR", 70000, 9),
+("Customer Service Rep", 40000, 8),
+("IT", 100000, 9);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Scott", "Davis", 1, null),
+("Bob", "Lee", 2, 1),
+("Rob", "Stark", 3, 1),
+("Andy", "Bernard", 4, 2),
+
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
